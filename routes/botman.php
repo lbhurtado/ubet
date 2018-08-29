d0034243f0da2ceca18684bdbd0423ba66d98b85
Lester Hurtado <lester@Lesters-MacBook-Pro-4.local>
2018-08-29 20:10:29 +0800

    include DialogFlow Small Talk functionality as fallback

--- routes/botman.php
+++ routes/botman.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\BotManController;
+use BotMan\BotMan\Middleware\ApiAi;
+use BotMan\BotMan\BotMan;
 
 $botman = resolve('botman');
 
@@ -7,3 +9,9 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+
+$botman->middleware->received(ApiAi::create('a3642c76b1ef4cbbb9ea5a8a34fb9e48'));
+
+$botman->fallback(function (BotMan $bot){
+    return $bot->reply($bot->getMessage()->getExtras('apiReply'));
+});
